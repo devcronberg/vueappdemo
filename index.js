402f5f3fa99979b8f2e57e78d00db4430f68f909
Michell Cronberg <michell@cronberg.dk>
2020-10-16 18:23:46 +0200

    .

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import App from "./components/App";
-import { routes } from "./routes.js";
+import App from "/components/App";
+import { routes } from "/routes.js";
 
 Vue.use(VueRouter);
 const router = new VueRouter({
