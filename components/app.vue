a26d8233428c21b4de07fe60b63a0561be0100ab
Michell Cronberg <michell@cronberg.dk>
2020-10-17 10:16:00 +0200

    .

--- components/app.vue
+++ components/app.vue
@@ -2,31 +2,31 @@
   <div>
     <h1>Beregninger</h1>
     <div>
-      [<router-link to="/" active-class="active" exact><a>Forside</a></router-link>]
-      [<router-link to="/firkant" active-class="active"><a>Firkant</a></router-link>] 
-      [<router-link to="/trekant" active-class="active"><a>Trekant</a></router-link>]</div>
-    
+      [<router-link to="/" active-class="active" exact><a>Forside</a></router-link
+      >] [<router-link to="/firkant" active-class="active"><a>Firkant</a></router-link
+      >] [<router-link to="/trekant" active-class="active"><a>Trekant</a></router-link
+      >]
+    </div>
+
     <router-view></router-view>
-    
   </div>
 </template>
 
 <script>
 import Vue from "vue";
-import arealfirkant from "./arealFirkant.vue";
-import arealtrekant from "./arealTrekant.vue";
-
+import arealfirkant from "~/components/arealFirkant.vue";
+import arealtrekant from "~/components/arealTrekant.vue";
 
 export default Vue.extend({
   components: {
     arealfirkant: arealfirkant,
-    arealtrekant: arealtrekant
-  }
+    arealtrekant: arealtrekant,
+  },
 });
 </script>
 
 <style scoped>
-  .active{
-    background-color: lightblue;
-  }
+.active {
+  background-color: lightblue;
+}
 </style>
